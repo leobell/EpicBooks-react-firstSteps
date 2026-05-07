85e45f23d2d912566fcb532a31da72618134846b
leobell <bellleonardo04@gmail.com>
2026-05-07 14:51:05 +0200

    Refactor book display: replace MyBookCard with SingleBook component and add search functionality

--- src/components/allTheBooks/AllTheBooks.jsx
+++ src/components/allTheBooks/AllTheBooks.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FantasyBooks from '../../books/fantasy.json'
-import MyBookCard from '../myBookCard/MyBookCard'
+import SingleBook from '../singleBook/SingleBook'
 
 const AllTheBooks = () => {
+
+  const [searchInput, setSearchInput] = useState('')
+  const filterBook = FantasyBooks.filter(book => book.title.toLowerCase().includes(searchInput.toLowerCase()))
+
+  console.log(searchInput)
+
   return (
     <div className='row'>
+      <form className=' mb-4'>
+        <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+          className='form-control'
+          placeholder='cerca libro'
+          type="text"
+        />
+      </form>
       {
-        FantasyBooks.map((book) => (
-            <MyBookCard 
+        filterBook.map((book) => (
+            
+            <SingleBook 
                 key={book.asin}
                 img={book.img}
                 title={book.title}
--- src/components/singleBook/SingleBook.jsx
+++ src/components/singleBook/SingleBook.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from 'react-bootstrap/Card'
 
+const SingleBook = (props) => {
+  const [selected, setSelected] = useState(false)
 
+  const onSelected = () => {
+    setSelected(!selected)
+  }
 
-const MyBookCard = (props) => {
+  //console.log(selected)
+
   return (
     <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
-      <Card style={{ width: '100%' }}>
+      <Card 
+        onClick={onSelected}
+        style={{ width: '100%' , border: selected ? "4px solid green" : "none"}}
+      >
         <Card.Img variant="top" src={props.img} style={{aspectRatio: '1', objectFit: 'contain'}}/>
         <Card.Body>
           <Card.Title 
@@ -26,4 +35,4 @@ const MyBookCard = (props) => {
   )
 }
 
-export default MyBookCard
+export default SingleBook
